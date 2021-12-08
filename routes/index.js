ed3b0505feb591f8f6267b70b9ae7c0cee093450
Enzo RAG <enzogroisard@hotmail.fr>
2021-12-08 22:50:17 +0100

    testheroke2012121212121212121212121

--- routes/index.js
+++ routes/index.js
@@ -82,6 +82,8 @@ router.post('/addrdv',async function(req, res){
         console.log('forrrrrrr',test)
         var test3 = await UserModel.findById(test.patientId)
         console.log("testttttttttttttttt", test3.nom)
+        var test4 = {name : test3.nom}
+        console.log("testtetetetetetete",test4)
         // var test4 = Object.assign(test, test3)
       }
 
