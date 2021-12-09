b55ffc34d4d6e7af537f21885a0e123257b4b621
Lucas Delamare <lucas.delamare84@gmail.com>
2021-12-09 16:58:15 +0100

    création d'un user tah les fou8

--- routes/index.js
+++ routes/index.js
@@ -397,7 +397,7 @@ var dispoDate = isAvailableAleatoire()
   router.get('/seeder-user',async function(req, res, next){
     for (var i=0;i<5;i++){
      creerUser()
-     console.log(i)}
+     console.log("*******************************************************************************",i)}
     res.json({ok:"ok"})
   })
   
