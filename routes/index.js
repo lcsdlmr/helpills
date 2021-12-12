64f05b5f29dfed69443c431625d6f70a34caaa21
Enzo RAG <enzogroisard@hotmail.fr>
2021-12-12 21:55:26 +0100

    heroku test route add presciption121

--- routes/index.js
+++ routes/index.js
@@ -166,13 +166,13 @@ router.post('/addrdv',async function(req, res){
       router.post('/addprescription',async function(req, res){
         console.log("route add info##########################")
         
-        var prescription = await RdvModel.updateOne({_id : req.body.id},{$push:{
+        var prescription = await RdvModel.updateOne({ _id : req.body.id},{$push:{
           number: req.body.number,
           prise: req.body.prise,
           duree: req.body.duree,
           autre: req.body.autre,}})
         
-      
+          console.log(prescription)
         
         
         res.json({prescription});
